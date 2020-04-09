a7148f1e23e642d52638ab7aa24d001157947ec1
Nyambayar S <web-master@tavanbogd.com>
2020-04-09 13:05:46 +0800

    Save

--- index.js
+++ index.js
@@ -10,8 +10,18 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'))
 });
 
+app.get('/drink', (req, res) => {
+    res.sendFile(path.join(__dirname + '/drink.html'))
+});
+
 app.get('/food-count', (req, res) => {
     fs.readdir('img/food', (error, files) => {
+        res.status(200).json({ count: files.length })
+    });
+});
+
+app.get('/drink-count', (req, res) => {
+    fs.readdir('img/drink', (error, files) => {
         res.status(200).json({ count: files.length })
     });
 });
