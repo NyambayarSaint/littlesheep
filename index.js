bd5924e48e8cad2f0ae76b7fc679b6fff24ffae3
Nyambayar S <web-master@tavanbogd.com>
2020-04-09 12:32:03 +0800

    Save

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 5000;
+const fs = require('fs');
+
+app.use("/img", express.static(__dirname + '/img'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'))
+});
+
+app.get('/food-count', (req, res) => {
+    fs.readdir('img/food', (error, files) => {
+        res.status(200).json({ count: files.length })
+    });
+});
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+});
